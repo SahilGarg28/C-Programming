bb49930a31c6894036de226c4ccff8bdafffc525
Sahil Garg <69622920+SahilGarg28@users.noreply.github.com>
2021-02-14 22:32:43 +0530

    Update who_i_am.c

--- who_i_am.c
+++ who_i_am.c
@@ -4,8 +4,7 @@ int main ()
   printf ("My name is Sahil Garg.\n");
   printf ("I am 1st year student learning from Chandigarh University.\n");
   printf ("My hobbies are painting, poster making, collage making also.\n");
-  printf
-    ("I love to play with computers.As I wrote about me by using C code and this is my first C program\n ");
+  printf("I love to play with computers.As I wrote about me by using C code and this is my first C program\n ");
 
   return 0;
 }
