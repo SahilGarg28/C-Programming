24e0f16faa7cc0e4e9cf6ee264a17ba13a22919c
Sahil Garg <69622920+SahilGarg28@users.noreply.github.com>
2021-03-01 01:04:38 +0530

    Update leap_year_or_not.c

--- leap_year_or_not.c
+++ leap_year_or_not.c
@@ -3,7 +3,7 @@ int main()
 { int year;
     printf("Enter any year: ");
     scanf("%d",&year);
-    (year%4==0?printf("%d is a leap year",year):printf("%d is not a leap year.",year));
+    (year%4==0&&year%100==0||year%400==0?printf("%d is a leap year",year):printf("%d is not a leap year.",year));
     
     return 0;
 }
