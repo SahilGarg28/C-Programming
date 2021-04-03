007f7595b54648fe6e924a4294ce9053f80bd41b
Sahil Garg <69622920+SahilGarg28@users.noreply.github.com>
2021-04-03 14:46:54 +0530

    Create pattern_1.c

---
+++ pattern_1.c
@@ -0,0 +1,31 @@
+/*
+Enter N: 5
+r       b       r       r       b       r
+r       r       b       r       r        
+b       r       r       b
+r       b       r
+r       r
+b
+*/
+
+#include<stdio.h>
+int main()
+{
+    int n,r,c,x=1;
+    printf("Enter N: ");//3
+    scanf("%d",&n);
+    
+    for(r=n;r>=0;r--)
+    {
+        for(c=0;c<=r;c++)
+        {
+
+                if((c+r)%3==0)
+                printf("b\t");
+                else
+                printf("r\t");
+            
+        }
+        printf("\n");
+    }
+}
